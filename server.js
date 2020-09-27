2e4c4a1b892fbaae5112e4c808f96cf62e0f7778
Tracy Le <tnle9@asu.edu>
2020-09-27 16:11:13 -0700

    Checking PORT

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const cors = require("cors");
 const db = require("./models");
 const NODEPORT  =  3001;
 
-
+console.log("PORT" + process.env.PORT);
 // Configure middleware
 // Set up express to use this port
 app.set("port", process.env.PORT || NODEPORT);
